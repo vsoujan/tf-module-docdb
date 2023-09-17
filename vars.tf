8115c32d3653d1eeb3aa0a75217c1575bf5acbda
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-09-17 13:47:29 +0530

    parameters

--- vars.tf
+++ vars.tf
@@ -1,3 +1,9 @@
 variable "tags" {}
 variable "env" {}
 variable "subnet_ids" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "skip_final_snapshot" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+variable "engine_version" {}
