ca36d5a107bee816418203ab52c772c18dc9ebc5
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-09-17 14:18:08 +0530

    parameters

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_docdb_cluster_parameter_group" "main" {
-  family      = var.engine_family
+  family      = "docdb4.0"
   name        = "${local.name_prefix}-pg"
   description = "${local.name_prefix}-pg"
   tags        = merge(local.tags, { Name = "${local.name_prefix}-pg" })
